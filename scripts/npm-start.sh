43483d1e211f49ce00f9a3adb78894ce716b7478
Emil Petersson <emil.petersson@jayway.com>
2018-09-14 14:24:09 +0200

    source before each script is run

--- scripts/npm-start.sh
+++ scripts/npm-start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash'
 
 echo "npm-start initiated..."
+source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/
 
--- scripts/npm-stop.sh
+++ scripts/npm-stop.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "npm-stop initiated..."
+source /home/ec2-user/.bash_profile
 
 if which node > /dev/null
   then
