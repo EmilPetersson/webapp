868d2aa04fe80d716eed186a48edb5c21d2997d5
Emil Petersson <emil.petersson@jayway.com>
2018-09-14 13:50:08 +0200

    fix

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -4,10 +4,7 @@ echo "npm-install initiated..."
 
 cd /home/ec2-user/
 
-if [ -d "/node_modules/" ]
-  then
-    rm -rf node_modules/
-fi
+sudo rm -rf /home/ec2-user/node_modules/
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 
