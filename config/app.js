f702ed301d162376d7d0900c676f5eb63b0fe906
Emil Petersson <emil.petersson@jayway.com>
2018-09-18 10:23:46 +0200

    added ejs files

--- config/app.js
+++ config/app.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var app = express();
+
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-  res.send('Hello world(test2)!\n');
-})
+	res.render('pages/index');
+});
+
+app.get('/about', function(req, res) {
+	res.render('pages/about');
+});
+
+
 var port = 8080;
 app.listen(port);
 console.log('Listening on port', port);
